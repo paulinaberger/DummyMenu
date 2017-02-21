f9e7eb555bf22a2b9619938b7173894dc2519a1d
Paulina Berger <paulina.berger@gmail.com>
2017-02-21 10:09:28 +0100

    added a menu

--- app/src/main/java/org/mdsd2016/android/dummymenu/MainActivity.java
+++ app/src/main/java/org/mdsd2016/android/dummymenu/MainActivity.java
@@ -2,12 +2,17 @@ package org.mdsd2016.android.dummymenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //this method inflates the layout
+        //when doing menus, you have to do an inflator yourself
+
+        MenuInflater mMenuInflater = this.getMenuInflater();
+        mMenuInflater.inflate(R.menu.menu_main, menu);
     }
 }

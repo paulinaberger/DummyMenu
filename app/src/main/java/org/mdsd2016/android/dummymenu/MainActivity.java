faaa2313f45acad474dde0f9aaed90af0cf82490
Paulina Berger <paulina.berger@gmail.com>
2017-02-21 13:04:53 +0100

    //three menus in app so far     //regular options menu that can be shown with the menu button in all devices     //second one includes the contextual action menu(floating in the  middle)     //third one - is the contextual action bar
        //all three use the same layout
        //options menu shouldn't be the same as the contextual action menu

--- app/src/main/java/org/mdsd2016/android/dummymenu/MainActivity.java
+++ app/src/main/java/org/mdsd2016/android/dummymenu/MainActivity.java
@@ -2,6 +2,7 @@ package org.mdsd2016.android.dummymenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -10,7 +11,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActionMode.Callback, View.OnLongClickListener{
+//three menus in app so far
+    //regular options menu that can be shown with the menu button in all devices
+    //second one includes the contextual action menu(floating in the  middle)
+    //third one - is the contextual action bar
+
+    //all three use the same layout
+    //options menu shouldn't be the same as the contextual action menu
 
     private static final String TAG_MAIN_ACTIVITY = "In-MainActivity";
 
@@ -21,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); //this method inflates the layout
         //when doing menus, you have to do an inflator yourself
 
-        final ImageView imageViewIcon = (ImageView) findViewById(R.id.image_view_icon_mainactivity);
-        registerForContextMenu(imageViewIcon);
+        final ImageView imageViewIconBottom = (ImageView) findViewById(R.id.image_view_icon_bottomimageView2);
+        imageViewIconBottom.setOnLongClickListener(this);
 
     }
 
@@ -99,7 +107,50 @@ public class MainActivity extends AppCompatActivity {
         return super.onContextItemSelected(item);
 
      }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu); // inflating menu
+        return true;
+
+        //we haven't defined which item has to be long clicked
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View whichView) {
+
+        if(whichView.getId() == R.id.image_view_icon_bottomimageView2){
+            startSupportActionMode(this);
+            return true;
+
+        }
+        return false;
+
+    }
 }
+
+//two things happened
+//floating action mode was onCreate
+//actionmode bar - on top of app, need to define things manually, that's why we have two interfaces
+//actionmode allows to implement callbacks.
+//when we implemented the actionMode - we had option to implement from support library and from a non-support library
+//difference between libraries is the support based on versions
 
 
 
